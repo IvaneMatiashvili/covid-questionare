19f598a4ee1a07e78e752b54f18e5d22fc55ba59
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-18 01:40:36 +0400

    feat: create a routes for pages and connect tailwind css to index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from 'react';
+import LandingPage from '/src/pages/LangingPage/LandingPage.jsx';
+import '/src/index.css';
+import { Route, Routes } from 'react-router-dom';
+import NotFoundPage from '/src/pages/NotFoundPage/NotFoundPage';
+import IdentityPage from '/src/pages/IdentityPage/IdentityPage';
+import CovidQuestionnairePage from '/src/pages/CovidQuestionnairePage/CovidQuestionnairePage.jsx';
+import VaccinationPage from '/src/pages/VaccinationPage/ VaccinationPage';
+import TipsPage from '/src/pages/TipsPage/TipsPage.jsx';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className='bg-red-100 w-[40rem] h-[20rem]'>
-      <h1 className={''}>some text</h1>
-      <h1></h1>
-
-      <h1></h1>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/identity' element={<IdentityPage />} />
+        <Route
+          path='/covid-questionnaire'
+          element={<CovidQuestionnairePage />}
+        />
+        <Route path='/vaccination' element={<VaccinationPage />} />
+        <Route path='/tips' element={<TipsPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
