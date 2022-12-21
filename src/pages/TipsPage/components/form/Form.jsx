50253009d599c01012a2b8713c49dbd940731dee
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-21 19:47:33 +0400

    fix: remove a unnecessary code

--- src/pages/TipsPage/components/form/Form.jsx
+++ src/pages/TipsPage/components/form/Form.jsx
@@ -1,123 +1,23 @@
-import MeetingField from '@/pages/TipsPage/components/form/MeetingField';
-import WorkInOfficeField from '@/pages/TipsPage/components/form/WorkInOfficeField';
-import PhysicalMeetingsField from '@/pages/TipsPage/components/form/PhysicalMeetingsField';
-import WhatWouldYouChangeField from '@/pages/TipsPage/components/form/WhatWouldYouChangeField';
-import { useForm, useWatch } from 'react-hook-form';
+import MeetingField from '@/pages/TipsPage/components/form/MeetingField.jsx';
+import WorkInOfficeField from '@/pages/TipsPage/components/form/WorkInOfficeField.jsx';
+import PhysicalMeetingsField from '@/pages/TipsPage/components/form/PhysicalMeetingsField.jsx';
+import WhatWouldYouChangeField from '@/pages/TipsPage/components/form/WhatWouldYouChangeField.jsx';
 import { ErrorMessage } from '@hookform/error-message';
-import { useContext, useEffect } from 'react';
-import SendDataContext from '@/context/send-data-context';
-import getRegisterRequest from '@/services/index';
-import LeftArrow from '@/components/icons/LeftArrow';
-import { useNavigate } from 'react-router-dom';
+import LeftArrow from '@/components/icons/LeftArrow.jsx';
+import { useTipsPageForm } from '@/pages/TipsPage/components/form/useTipsPageForm.jsx';
 
 const Form = () => {
-  const navigate = useNavigate();
-  const {
-    identity: { name, lastName, email },
-    covidQuestionnaire: {
-      haveCovid,
-      haveAntibodies,
-      covidSicknessDate,
-      antibodiesQuantity,
-      testDate,
-    },
-    vaccination: { haveVaccination, stage, whatAreYouWaitingFor },
-    tips: {
-      meetingField,
-      setMeetingField,
-      workInOfficeField,
-      setWorkInOfficeField,
-      physicalMeetingsField,
-      setPhysicalMeetingsField,
-      whatWouldYouChangeField,
-      setWhatWouldYouChangeField,
-    },
-  } = useContext(SendDataContext);
-  const ctx = useContext(SendDataContext);
-
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({
-    defaultValues: {
-      meetingField: localStorage.getItem('meetingField') || '',
-      workInOfficeField: localStorage.getItem('workInOfficeField') || '',
-      physicalMeetingsField:
-        localStorage.getItem('physicalMeetingsField') || '',
-      whatWouldYouChangeField:
-        localStorage.getItem('whatWouldYouChangeField') || '',
-    },
-  });
-  const watchMeetingField = useWatch({
-    control,
-    name: 'meetingField',
-  });
-  const watchWorkInOfficeField = useWatch({
-    control,
-    name: 'workInOfficeField',
-  });
-  const watchPhysicalMeetingsField = useWatch({
-    control,
-    name: 'physicalMeetingsField',
-  });
-  const watchWhatWouldYouChangeField = useWatch({
-    control,
-    name: 'whatWouldYouChangeField',
-  });
-
-  useEffect(() => {
-    localStorage.setItem('meetingField', watchMeetingField);
-    localStorage.setItem('workInOfficeField', watchWorkInOfficeField);
-    localStorage.setItem('physicalMeetingsField', watchPhysicalMeetingsField);
-    localStorage.setItem(
-      'whatWouldYouChangeField',
-      watchWhatWouldYouChangeField
-    );
-
-    setMeetingField(watchMeetingField);
-    setWorkInOfficeField(watchWorkInOfficeField);
-    setPhysicalMeetingsField(watchPhysicalMeetingsField);
-    setWhatWouldYouChangeField(watchWhatWouldYouChangeField);
-  }, [
-    watchMeetingField,
-    watchWorkInOfficeField,
-    watchPhysicalMeetingsField,
-    watchWhatWouldYouChangeField,
-  ]);
-
+  const { register, errors, handleSubmit, submit, navigateLeft } =
+    useTipsPageForm();
   return (
-    <form
-      onSubmit={handleSubmit((data) => {
-        const registerRequest = getRegisterRequest({
-          name,
-          lastName,
-          email,
-          haveCovid,
-          haveAntibodies,
-          covidSicknessDate,
-          antibodiesQuantity,
-          testDate,
-          haveVaccination,
-          stage,
-          whatAreYouWaitingFor,
-          meetingField,
-          workInOfficeField,
-          physicalMeetingsField,
-          whatWouldYouChangeField,
-        });
-        localStorage.setItem('page', '5');
-        navigate('/thank-you', { replace: true });
-      })}
-    >
+    <form onSubmit={handleSubmit(submit)}>
       <div className='mt-[2.6rem] mb-[8rem]'>
         <MeetingField register={register} />
         <ErrorMessage
           errors={errors}
           name='meetingField'
           render={({ message }) => (
-            <p className='absolute mt-2 ml-4 font-normal text-base text-text-error'>
+            <p className='absolute font-arial mt-2 ml-4 font-normal text-base text-text-error'>
               {message}
             </p>
           )}
@@ -127,7 +27,7 @@ const Form = () => {
           errors={errors}
           name='workInOfficeField'
           render={({ message }) => (
-            <p className='absolute mt-2 ml-4 font-normal text-base text-text-error'>
+            <p className='absolute font-arial mt-2 ml-4 font-normal text-base text-text-error'>
               {message}
             </p>
           )}
@@ -137,7 +37,7 @@ const Form = () => {
         <div className='w-[38rem] flex justify-end mt-[2.5rem]'>
           <button
             type='submit'
-            className='bg-btn-green w-[10rem] h-[3.5rem] z-20 font-bold text-base text-white rounded-[2.6rem] flex justify-center items-center'
+            className='bg-btn-green w-[10rem] font-arial h-[3.5rem] z-20 font-bold text-base text-white rounded-[2.6rem] flex justify-center items-center'
           >
             დასრულება
           </button>
@@ -145,10 +45,7 @@ const Form = () => {
       </div>
       <div
         className='absolute left-[50%] top-[181%] z-20 cursor-pointer'
-        onClick={() => {
-          localStorage.setItem('page', '3');
-          navigate('/vaccination', { replace: true });
-        }}
+        onClick={navigateLeft}
       >
         <LeftArrow />
       </div>
