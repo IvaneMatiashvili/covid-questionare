061b2f0bce70f25c6ff5458132855514b0493440
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-21 19:38:56 +0400

    feat: add animations to covid-questuonnaire page

--- src/pages/CovidQuestionnairePage/CovidQuestionnairePage.jsx
+++ src/pages/CovidQuestionnairePage/CovidQuestionnairePage.jsx
@@ -1,27 +1,26 @@
 import Layout from '@/components/Layout';
-import Form from '@/pages/CovidQuestionnairePage/components/Form';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import Form from '@/pages/CovidQuestionnairePage/components/form/Form.jsx';
+import useRedirectToCurrentPage from '@/hooks/use-redirect-to-current-page';
+import '@/css/covid-questionnaire-page-animation.css';
 
 const CovidQuestionnairePage = () => {
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem('page') !== '2') {
-      navigate(-1);
-    }
-  });
+  useRedirectToCurrentPage();
   return (
     <>
       <Layout>
-        <div className='w-[95rem] flex justify-between overflow-hidden'>
+        <div className='w-[95rem] flex justify-between overflow-hidden opacity-animation'>
           <div>
             <Form />
           </div>
           <img
             src='/assets/covid-questionnaire.png'
-            className='object-top object-cover h-[48rem]'
+            className='object-top object-cover z-10 h-[48rem]'
+          />
+          <img
+            src='/assets/circle.png'
+            className={`absolute left-[50%] top-[33%] covid-questionnaire-logo-from-${localStorage.getItem(
+              'from'
+            )}-animation`}
           />
         </div>
       </Layout>
