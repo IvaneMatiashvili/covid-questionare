f8227d20fc2bd615d81ed60bce135e387c1049c6
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-21 19:48:51 +0400

    feat: add a animations to vaccination page

--- src/pages/VaccinationPage/ VaccinationPage.jsx
+++ src/pages/VaccinationPage/ VaccinationPage.jsx
@@ -1,30 +1,28 @@
 import Layout from '/src/components/Layout.jsx';
-import Form from '/src/pages/VaccinationPage/components/Form';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import Form from '/src/pages/VaccinationPage/components/form/Form';
+import useRedirectToCurrentPage from '@/hooks/use-redirect-to-current-page';
+import '@/css/vaccination-page-animation.css';
 
 const VaccinationPage = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem('page') !== '3') {
-      navigate(-1);
-    }
-  });
+  useRedirectToCurrentPage();
   return (
-    <>
-      <Layout>
-        <div className='w-[95rem] flex justify-between overflow-hidden'>
-          <div>
-            <Form />
-          </div>
-          <img
-            src='/assets/vaccination.png'
-            className='object-top object-cover h-[48rem]'
-          />
+    <Layout>
+      <div className='w-[95rem] flex justify-between overflow-hidden opacity-animation'>
+        <div>
+          <Form />
         </div>
-      </Layout>
-    </>
+        <img
+          src='/assets/vaccination.png'
+          className='object-top z-10 object-cover h-[48rem]'
+        />
+        <img
+          src='/assets/star.png'
+          className={`absolute left-[50%] top-[12%] vaccination-logo-from-${localStorage.getItem(
+            'from'
+          )}-animation`}
+        />
+      </div>
+    </Layout>
   );
 };
 
