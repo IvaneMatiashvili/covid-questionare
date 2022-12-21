e843af28983595b6c0e0c9038abb972846bb1308
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-21 19:46:40 +0400

    feat: add a animations to tips page

--- src/pages/TipsPage/TipsPage.jsx
+++ src/pages/TipsPage/TipsPage.jsx
@@ -1,40 +1,36 @@
 import Layout from '@/components/Layout';
-import Form from '@/pages/TipsPage/components/Form';
-import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import Form from '@/pages/TipsPage/components/form/Form.jsx';
+import useRedirectToCurrentPage from '@/hooks/use-redirect-to-current-page';
+import '@/css/tips-page-animation.css';
 
 const VaccinationPage = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem('page') !== '4') {
-      navigate(-1);
-    }
-  });
+  useRedirectToCurrentPage();
   return (
-    <>
-      <Layout>
-        <div className='w-[95rem] flex justify-between overflow-hidden'>
-          <div>
-            <p className='w-[28rem] font-normal text-base text-dark-100 mt-[2.5rem]'>
-              რედბერის მთავარი ღირებულება ჩვენი გუნდის თითოეული წევრია. გარემო,
-              რომელსაც ჩვენი თანამშრომლები ქმნით, ბევრისთვის არის და ყოფილა
-              წლების განმავლობაში მიზნებისთვის ერთად ბრძოლის მიზეზი, ბევრისთვის
-              კი — ჩვენთან გადმოსვლის.
-            </p>
-            <p className='w-[27rem] font-normal text-base text-dark-100 mt-[2.5rem]'>
-              პანდემიის პერიოდში ერთმანეთსაც იშვიათად ვნახულობთ პირისპირ და
-              ყოველდღიური კომუნიკაციაც გაიშვიათდა.
-            </p>
-            <Form />
-          </div>
-          <img
-            src='/assets/tips.png'
-            className='object-top object-cover h-[48rem]'
-          />
+    <Layout>
+      <div className='w-[95rem] flex justify-between overflow-hidden opacity-animation'>
+        <div>
+          <p className='w-[28rem] font-arial font-normal text-base text-dark-100 mt-[2.5rem]'>
+            რედბერის მთავარი ღირებულება ჩვენი გუნდის თითოეული წევრია. გარემო,
+            რომელსაც ჩვენი თანამშრომლები ქმნით, ბევრისთვის არის და ყოფილა წლების
+            განმავლობაში მიზნებისთვის ერთად ბრძოლის მიზეზი, ბევრისთვის კი —
+            ჩვენთან გადმოსვლის.
+          </p>
+          <p className='w-[27rem] font-arial font-normal text-base text-dark-100 mt-[2.5rem]'>
+            პანდემიის პერიოდში ერთმანეთსაც იშვიათად ვნახულობთ პირისპირ და
+            ყოველდღიური კომუნიკაციაც გაიშვიათდა.
+          </p>
+          <Form />
         </div>
-      </Layout>
-    </>
+        <img
+          src='/assets/tips.png'
+          className='object-top object-cover z-10 fixed top-[20%] left-[50%] h-[48rem] pb-[5rem]'
+        />
+        <img
+          src='/assets/heart.png'
+          className={`fixed left-[55%] top-[25%] tips-logo-from-left-animation`}
+        />
+      </div>
+    </Layout>
   );
 };
 
