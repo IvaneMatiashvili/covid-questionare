99ed3f246c94b75f3d49d174fadf4ab2919c6918
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-21 19:44:48 +0400

    feat: add a animations to landing page

--- src/pages/LangingPage/LandingPage.jsx
+++ src/pages/LangingPage/LandingPage.jsx
@@ -1,32 +1,34 @@
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
+import useRedirectToCurrentPage from '@/hooks/use-redirect-to-current-page';
+import '@/css/landing-page-animation.css';
 
 const LandingPage = () => {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem('page') !== '0') {
-      navigate(-1);
-    }
-  });
+  useRedirectToCurrentPage();
   return (
-    <>
-      <div className='w-screen h-screen bg-soft-brown flex justify-center items-center'>
-        <div className='w-[10rem] flex flex-col justify-center items-center'>
-          <img src='/assets/redberry.png' />
+    <div className='w-screen h-screen bg-soft-brown flex justify-center items-center opacity-animation'>
+      <div className='flex flex-col justify-center items-center overflow-hidden'>
+        <img
+          src='/assets/redberry-full.png'
+          className=' absolute w-screen h-screen landing-full-logo-animation opacity-0'
+        />
+        <img src='/assets/redberry.png' className='landing-logo-animation' />
+        <div className='ml-[2px]'>
           <a
-            className='flex flex-col justify-center items-center cursor-pointer mt-[6.3rem]'
+            className='flex flex-col justify-center items-center cursor-pointer mt-[6.3rem] hover:bg-shadow bg-cover'
             onClick={() => {
               localStorage.setItem('page', '1');
               navigate('/identity', { replace: true });
             }}
           >
-            <spam className='font-bold text-[1.9rem]'>კითხვარის</spam>
-            <spam className='font-bold text-[1.9rem]'>დაწყება</spam>
+            <img
+              src='/assets/start.png'
+              className='relative right-[2px] start-questionnaire-animation'
+            />
           </a>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
