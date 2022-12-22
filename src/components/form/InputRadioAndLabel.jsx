d26eed0ff7a97984d12d5b038621fffaf5170c81
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-23 01:15:18 +0400

    feat: change a radio button colors

--- src/components/form/InputRadioAndLabel.jsx
+++ src/components/form/InputRadioAndLabel.jsx
@@ -3,22 +3,26 @@ import { useFormContext } from 'react-hook-form';
 const InputRadioAndLabel = ({ id, value, labelContent, name }) => {
   const { register } = useFormContext();
   return (
-    <div className='flex justify-start items-center ml-5 mt-4'>
-      <input
-        {...register(name, {
-          required: 'გთხოვთ მონიშნეთ პასუხი',
-        })}
-        id={id}
-        type='radio'
-        value={value}
-        className='w-[1.3rem] h-[1.3rem]'
-      />
-      <label
-        htmlFor={id}
-        className='ml-2 font-arial font-normal  text-xl font-medium text-dark-100'
-      >
-        {labelContent}
-      </label>
+    <div className='flex justify-start items-center ml-5 mt-2.5'>
+      <div className='h-8 flex justify-center items-center mr-4'>
+        <input
+          {...register(name, {
+            required: 'გთხოვთ მონიშნეთ პასუხი',
+          })}
+          id={id}
+          type='radio'
+          value={value}
+          className='w-[1.3rem] h-[1.3rem]'
+        />
+      </div>
+      <div className='h-8 flex justify-center items-center'>
+        <label
+          htmlFor={id}
+          className='ml-2 font-arial font-normal  text-xl font-medium text-dark-100'
+        >
+          {labelContent}
+        </label>
+      </div>
     </div>
   );
 };
