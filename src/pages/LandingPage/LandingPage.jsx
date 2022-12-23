8b4942d738f0ffadc76ef8c431a3a45000730073
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-23 15:19:34 +0400

    fix: home page logo animation

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,17 +1,20 @@
-import { useNavigation } from './useNavigation.js';
-import '@/css/landing-page-animation.css';
+import { useNavigation } from './useNavigation.js'
+import '@/css/landing-page-animation.css'
 
 const LandingPage = () => {
-  const { navigateToIdentityPage } = useNavigation();
+  const { navigateToIdentityPage } = useNavigation()
   return (
-    <div className='w-screen h-screen bg-soft-brown flex justify-center items-center opacity-animation'>
+    <div className='w-screen h-screen bg-soft-brown flex justify-center items-center overflow-hidden opacity-animation'>
       <div className='flex flex-col justify-center items-center overflow-hidden'>
-        <img
-          src='/assets/redberry-full.png'
-          className=' absolute w-screen h-screen landing-full-logo-animation opacity-0'
-        />
-        <img src='/assets/redberry.png' className='landing-logo-animation' />
-        <div className='ml-[2px]'>
+        <div className='absolute flex justify-center items-center overflow-hidden h-screen w-screen'>
+          <div className='absolute w-[200vw] h-[200vh] object-cover landing-full-logo overflow-hidden object-fill flex justify-center items-center mb-10'>
+            <img
+              src='/assets/redberry.png'
+              className='w-[7rem] h-[7rem] object-fill landing-full-logo-animation relative bottom-[5%] opacity-1'
+            />
+          </div>
+        </div>
+        <div className='ml-[2px] mt-6'>
           <a
             className='flex flex-col justify-center items-center cursor-pointer mt-[6.3rem] start-questionnaire-container-animation hover:bg-shadow bg-cover'
             onClick={navigateToIdentityPage}
@@ -24,7 +27,7 @@ const LandingPage = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LandingPage;
+export default LandingPage
