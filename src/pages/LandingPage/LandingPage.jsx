1821c402925193fe9b9087b6311b4b5c3239b10b
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-24 14:26:03 +0400

    fix: fix a home page animation bug

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,7 +2,7 @@ import { useNavigation } from './useNavigation.js'
 import '@/css/landing-page-animation.css'
 
 const LandingPage = () => {
-  const { navigateToIdentityPage } = useNavigation()
+  const { navigateToIdentityPage, hasHover, isPointer } = useNavigation()
   return (
     <div className='w-screen h-screen bg-soft-brown flex justify-center items-center overflow-hidden opacity-animation'>
       <div className='flex flex-col justify-center items-center overflow-hidden'>
@@ -16,16 +16,23 @@ const LandingPage = () => {
         </div>
         <div className='mt-6'>
           <a
-            className='flex flex-col justify-center overflow-hidden items-center h-16 w-52 cursor-pointer mt-[6.3rem] start-questionnaire-container-animation'
+            className={`flex flex-col justify-center overflow-hidden items-center h-16 w-52 ${
+              isPointer && 'cursor-pointer'
+            } mt-[6.3rem] start-questionnaire-container-animation`}
             onClick={navigateToIdentityPage}
           >
             <img
               src='/assets/start.png'
               className='relative start-questionnaire-animation h-16 w-52 object-contain'
             />
-            <div className='hover:bg-shadow bg-contain ml-12 bg-no-repeat shadow-container-animation absolute h-16 w-56'></div>
+            <div
+              className={`${
+                hasHover && 'hover:bg-shadow'
+              } bg-contain ml-12 bg-no-repeat shadow-container-animation absolute h-16 w-56`}
+            ></div>
           </a>
         </div>
+        <p>{hasHover}</p>
       </div>
     </div>
   )
--- src/pages/LandingPage/useNavigation.js
+++ src/pages/LandingPage/useNavigation.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 export const useNavigation = () => {
   const navigate = useNavigate()
@@ -6,7 +7,22 @@ export const useNavigation = () => {
     localStorage.setItem('page', '/identity')
     navigate('/identity')
   }
+
+  let [hasHover, setHasHover] = useState(false)
+  let [isPointer, setIsPointer] = useState(false)
+
+  const timer = setTimeout(() => {
+    setHasHover(true)
+    setIsPointer(true)
+  }, 800)
+
+  useEffect(() => {
+    return () => clearTimeout(timer)
+  }, [timer])
+
   return {
     navigateToIdentityPage,
+    hasHover,
+    isPointer,
   }
 }
