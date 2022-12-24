8272875121d9b76df579729dc1e41b5d51bd2901
IvaneMatiashvili <ivanematiashvili@gmail.com>
2022-12-24 14:56:57 +0400

    fix: use try catch to handle errors

--- src/services/index.js
+++ src/services/index.js
@@ -1,66 +1,67 @@
-import axios from 'axios';
-import moment from 'moment';
+import axios from 'axios'
+import moment from 'moment'
 
 const getRegisterRequest = async (enteredValues) => {
-  const data = {};
-  data['first_name'] = enteredValues.name;
-  data['last_name'] = enteredValues.lastName;
-  data['email'] = enteredValues.email;
-  data['had_covid'] = enteredValues.haveCovid;
+  const data = {}
+  data['first_name'] = enteredValues.name
+  data['last_name'] = enteredValues.lastName
+  data['email'] = enteredValues.email
+  data['had_covid'] = enteredValues.haveCovid
   if (enteredValues.haveCovid === 'yes') {
     if (enteredValues.haveAntibodies === 'no') {
-      data['had_antibody_test'] = false;
+      data['had_antibody_test'] = false
       let date = moment(new Date(enteredValues.covidSicknessDate)).format(
         'DD/MM/YYYY'
-      );
-      data['covid_sickness_date'] = date;
+      )
+      data['covid_sickness_date'] = date
     } else {
-      data['had_antibody_test'] = true;
-      const antibodies = {};
-      let count = 0;
+      data['had_antibody_test'] = true
+      const antibodies = {}
+      let count = 0
       if (enteredValues.testDate.length > 8) {
-        enteredValues.testDate = '';
-        antibodies['test_date'] = new Date(
-          enteredValues.testDate
-        ).toISOString();
-        count++;
+        enteredValues.testDate = ''
+        antibodies['test_date'] = new Date(enteredValues.testDate).toISOString()
+        count++
       }
       if (enteredValues.antibodiesQuantity.trim().length > 0) {
-        antibodies['number'] = +enteredValues.antibodiesQuantity;
-        count++;
+        antibodies['number'] = +enteredValues.antibodiesQuantity
+        count++
       }
       if (count > 0) {
-        data['antibodies'] = antibodies;
+        data['antibodies'] = antibodies
       }
     }
   }
 
   if (enteredValues.haveVaccination === 'yes') {
-    data['had_vaccine'] = true;
-    data['vaccination_stage'] = enteredValues.stage;
+    data['had_vaccine'] = true
+    data['vaccination_stage'] = enteredValues.stage
   } else {
-    data['had_vaccine'] = false;
-    data['i_am_waiting'] = enteredValues.whatAreYouWaitingFor;
+    data['had_vaccine'] = false
+    data['i_am_waiting'] = enteredValues.whatAreYouWaitingFor
   }
-  data['non_formal_meetings'] = enteredValues.meetingField;
-  data['number_of_days_from_office'] = enteredValues.workInOfficeField;
+  data['non_formal_meetings'] = enteredValues.meetingField
+  data['number_of_days_from_office'] = enteredValues.workInOfficeField
   if (enteredValues.physicalMeetingsField.trim().length > 0) {
-    data['what_about_meetings_in_live'] = enteredValues.physicalMeetingsField;
+    data['what_about_meetings_in_live'] = enteredValues.physicalMeetingsField
   }
   if (enteredValues.whatWouldYouChangeField.trim().length > 0) {
     data['tell_us_your_opinion_about_us'] =
-      enteredValues.whatWouldYouChangeField;
+      enteredValues.whatWouldYouChangeField
   }
-  const res = await axios({
-    method: 'post',
-    url: 'https://covid19.devtest.ge/api/create',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    data: JSON.stringify(data),
-  });
-  console.log(res);
-};
+  try {
+    const res = await axios({
+      method: 'post',
+      url: 'https://covid19.devtest.ge/api/create',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      data: JSON.stringify(data),
+    })
+  } catch (err) {
+    alert(err)
+  }
+}
 
-export default getRegisterRequest;
+export default getRegisterRequest
